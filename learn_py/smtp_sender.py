09bd05256f47cf60e157a77689e6b7bbc5ceb3be
michal19wrzes <michal19wrzes@gmail.com>
2022-01-17 22:41:40 +0100

    email sending smtp

--- learn_py/smtp_sender.py
+++ learn_py/smtp_sender.py
@@ -1,20 +1,20 @@
 import smtplib
 import os
-EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')     # mail to google account
-EMAIL_PASS = os.environ.get('EMAIL_PASS')           # pass to google account (configured lesssecureapp)
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')     # mail to google account (env)
+EMAIL_PASS = os.environ.get('EMAIL_PASS')           # pass to google account (env)
 
+def wyslij(body,subject = 'q(o.o)p'):
+    #send mail with body and subject
+    
+    with smtplib.SMTP('smtp.gmail.com',587) as smtp:
+        smtp.ehlo()                                     #identification
+        smtp.starttls()                                 #ciphering
+        smtp.ehlo()
+        
+        smtp.login(EMAIL_ADDRESS,EMAIL_PASS)
+         
+        msg = f'Subject: {subject}\n\n{body}'
+        smtp.sendmail(EMAIL_ADDRESS,'michal19wrzes@gmail.com',msg)  #(from, to, msg)
 
-with smtplib.SMTP('smtp.gmail.com',587) as smtp:
-    smtp.ehlo()                                     #identification
-    smtp.starttls()                                 #ciphering
-    smtp.ehlo()
-    
-    smtp.login(EMAIL_ADDRESS,EMAIL_PASS)
-    
-    subject = 'Testowy temat'
-    body = 'Ładny dzień tralal lala'
-    
-    msg = f'Subject: {subject}\n\n{body}'
-    smtp.sendmail(EMAIL_ADDRESS,EMAIL_ADDRESS,msg)  #(from, to, msg)
     
     
