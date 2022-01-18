cb2c513a2df1ed738ab5989b45a5fa5dfdcc5fed
michal19wrzes <michal19wrzes@gmail.com>
2022-01-18 23:45:08 +0100

    add,delete,show row in mysql

--- learn_py/crossProduct/crossProduct.py
+++ learn_py/crossProduct/crossProduct.py
@@ -9,7 +9,8 @@ root = Tk()
 root.resizable(width=False, height=True)
 
 # FUNKCJE OBSŁUGI OKNA
-def klikniętoPrzycisk() :
+def showQButton() :
+#action after click 'show question' button
   query="SELECT data,answer FROM question"
   f = ""
   cursor = mydb.cursor()
@@ -18,6 +19,20 @@ def klikniętoPrzycisk() :
     f = f + "{} = {}\n".format(id,name);  
   cursor.close()  
   txt.insert('2.0',f) #na początek
+def addQButton():
+#action after click 'add question' button
+    query="INSERT INTO question SET data ='{}', answer='{}'".format(questionEntry.get(),answerEntry.get())
+    cursor = mydb.cursor()
+    cursor.execute(query)
+def delQButton():
+#action after click 'delete button' --> for answer
+    query="DELETE FROM question WHERE answer='{}'".format(answerEntry.get())
+    cursor = mydb.cursor()
+    cursor.execute(query)
+def clearOutput():
+    txt.delete('1.0',END)
+ 
+
 
 ################ PROGRAM GŁÓWNY #######################
 
@@ -31,25 +46,42 @@ frame.grid(row=0, column=1 )
 frame.config(background='black')
 
 #NAPIS W RAMCE
-#lab1 = Label(frame,text='Ramka1') 
-#lab1.grid(sticky=E, row=0, column=0, padx=5, pady=5) 
+ 
 
-#POLE INPUT JEDNOLINIOWE 
-ent = Entry(frame, width=40)  # pole tekstowe jednoliniowe 
-ent.grid(sticky=W,row=1,column=0,padx=5, pady=5) 
-ent1 = Entry(frame, width=40)   
-ent1.grid(sticky=E,row=1,column=1,padx=5, pady=5) 
 
-#PRZYCISK
-przycisk = Button(frame,text='Pokaż pytania', command=klikniętoPrzycisk)
-przycisk.grid(row=4,column=1)
-przycisk.config(background='blue', foreground='#FFFF00')
+questionEntry = Entry(frame, width=40) 
+questionEntry.grid(sticky=W,row=2,column=1,padx=5, pady=5) 
+questionLabel = Label(frame,text='Wprowadź pytanie:') 
+questionLabel.grid(sticky=E, row=1, column=1, padx=5, pady=5)
+
+answerEntry = Entry(frame, width=40)   
+answerEntry.grid(sticky=E,row=4,column=1,padx=5, pady=5)
+answerLabel = Label(frame,text='Wprowadź odpowiedź:') 
+answerLabel.grid(sticky=E, row=3, column=1, padx=5, pady=5) 
+
+#Pokaż pytania button
+showQButton = Button(frame,text='Pokaż pytania', command=showQButton)
+showQButton.grid(row=4,column=2)
+showQButton.config(background='blue', foreground='#FFFF00')
+#Dodaj pytanie button
+addQButton = Button(frame,text='Dodaj pytanie', command=addQButton)
+addQButton.grid(row=3,column=2)
+addQButton.config(background='blue', foreground='#FFFF00')
+#Usun pytanie po hasle button
+delQButton = Button(frame,text='Usuń pytanie po haśle', command=delQButton)
+delQButton.grid(row=2,column=2)
+delQButton.config(background='blue', foreground='#FFFF00')
+#Wyczysc output button
+clearEntryButton = Button(frame,text='Wyczyść output', command=clearOutput)
+clearEntryButton.grid(row=1,column=2)
+clearEntryButton.config(background='blue', foreground='#FFFF00')
+
 
 
 
 #POLE TEKSTOWE WIELOLINIOWE 
-txt = Text(frame,width=40, height=5)
-txt.grid(sticky=W+N, row=4,column=0, padx=5, pady=5) 
+txt = Text(frame,width=100, height=20)
+txt.grid(sticky=W+N, row=0,column=0, padx=5, pady=5) 
 
 root.mainloop()
 mydb.close()
