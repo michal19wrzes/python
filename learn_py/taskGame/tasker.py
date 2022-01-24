7758f4e34d5c2a2005da15bb4dcd9173ae52302f
michal19wrzes <michal19wrzes@gmail.com>
2022-01-24 21:30:10 +0100

    menu preparation

--- learn_py/taskGame/tasker.py
+++ learn_py/taskGame/tasker.py
@@ -23,116 +23,127 @@ root.resizable(width=False, height=False)
 #font
 helv36 = tkFont.Font(family='Courier', size=13, weight='bold')
 
-def executeSortPriority():
+
+def testowo():
+    def executeSortPriority():
     #insert priority querry result to output entry
-    executeClearOutput()
-    sortPriority(mydb,txtArea)    
-def executeRandomTask():
+        executeClearOutput()
+        sortPriority(mydb,txtArea)    
+    def executeRandomTask():
     #insert randomTask querry result to output entry
-    randomTask(mydb,txtArea)#na początek  
-def executeUpdateTask():
+        randomTask(mydb,txtArea)#na początek  
+    def executeUpdateTask():
     #update task by the entered params where id = idTaskEntry
-    updateTask(mydb,taskEntry,priorityEntry,statusEntry,idTaskEntry)   
-def executeShowTOffButon():
+        updateTask(mydb,taskEntry,priorityEntry,statusEntry,idTaskEntry)   
+    def executeShowTOffButon():
     #show disabled tasks
-    executeClearOutput()
-    showTOffButon(mydb,txtArea)    
-def executeShowTOnButon():
+        executeClearOutput()
+        showTOffButon(mydb,txtArea)    
+    def executeShowTOnButon():
     #show enabled tasks
-    executeClearOutput()
-    showTOnButon(mydb,txtArea)  
-def executeChangeStatus():
+        executeClearOutput()
+        showTOnButon(mydb,txtArea)  
+    def executeChangeStatus():
     #change status (0|1) task 
-    changeStatus(mydb,idTaskEntry)  
-def executeShowTButon():
+        changeStatus(mydb,idTaskEntry)  
+    def executeShowTButon():
     #show all tasks and insert to txtArea
-    executeClearOutput()    
-    showTButon(mydb,txtArea)  
-def executeAddTButon():
+        executeClearOutput()    
+        showTButon(mydb,txtArea)  
+    def executeAddTButon():
     #add task to db
-    addTButon(mydb,taskEntry,statusEntry,priorityEntry)  
-def executeDelTButon():
+        addTButon(mydb,taskEntry,statusEntry,priorityEntry)  
+    def executeDelTButon():
     #delete task from db
-    delTButon(mydb,idTaskEntry)  
-def executeClearOutput():
+        delTButon(mydb,idTaskEntry)  
+    def executeClearOutput():
     #delete inserted data task from output and input textfield
-    clearOutput(txtArea,taskEntry,priorityEntry,statusEntry,idTaskEntry)
-
-   
-#ENTRYS
-frame = Frame(root,borderwidth=4)  
-frame.grid(row=0, column=1 )
-frame.config(background='black')
+        clearOutput(txtArea,taskEntry,priorityEntry,statusEntry,idTaskEntry)
 
 
-taskEntry = Entry(frame, width=40) 
-taskEntry.grid(sticky=N,row=2,column=0,padx=5, pady=5) 
-taskLabel = Label(frame,text='Wprowadź zadanie:') 
-taskLabel.grid(sticky=N, row=1, column=0, padx=5, pady=5)
+    #ENTRYS
+    frame = Frame(root,borderwidth=4)  
+    frame.grid(row=0, column=1 )
+    frame.config(background='black')
 
-priorityEntry = Entry(frame, width=40)   
-priorityEntry.grid(sticky=N,row=2,column=1,padx=5, pady=5)
-priorityLabel = Label(frame,text='Wprowadź priorytet (1-100):') 
-priorityLabel.grid(sticky=N, row=1, column=1, padx=5, pady=5)
 
-statusEntry = Entry(frame, width=40)   
-statusEntry.grid(sticky=N,row=2,column=2,padx=5, pady=5)
-statusLabel = Label(frame,text='Wprowadź status (1-ON | 0-OFF):') 
-statusLabel.grid(sticky=N, row=1, column=2, padx=5, pady=5) 
+    taskEntry = Entry(frame, width=40) 
+    taskEntry.grid(sticky=N,row=2,column=0,padx=5, pady=5) 
+    taskLabel = Label(frame,text='Wprowadź zadanie:') 
+    taskLabel.grid(sticky=N, row=1, column=0, padx=5, pady=5)
 
-idTaskEntry = Entry(frame, width=40)   
-idTaskEntry.grid(sticky=N,row=2,column=3,padx=5, pady=5)
-idTaskLabel = Label(frame,text='Wprowadź ID zadania:') 
-idTaskLabel.grid(sticky=N, row=1, column=3, padx=5, pady=5) 
+    priorityEntry = Entry(frame, width=40)   
+    priorityEntry.grid(sticky=N,row=2,column=1,padx=5, pady=5)
+    priorityLabel = Label(frame,text='Wprowadź priorytet (1-100):') 
+    priorityLabel.grid(sticky=N, row=1, column=1, padx=5, pady=5)
 
-#BUTTONS
-#show tasks button
-showTButton = Button(frame,text='Pokaż pytania', command=executeShowTButon)
-showTButton.grid(row=3,column=0)
-showTButton.config(background='green', foreground='#FFFF00')
-#show ON
-showTOnButton = Button(frame,text='Pokaż włączone', command=executeShowTOnButon)
-showTOnButton.grid(row=3,column=1)
-showTOnButton.config(background='green', foreground='#FFFF00')
-#show OFF
-showTOffButton = Button(frame,text='Pokaż wyłączone', command=executeShowTOffButon)
-showTOffButton.grid(row=3,column=2)
-showTOffButton.config(background='green', foreground='#FFFF00')
-#add task button
-addTButton = Button(frame,text='Dodaj zadanie', command=executeAddTButon)
-addTButton.grid(row=4,column=0)
-addTButton.config(background='blue', foreground='#FFFF00')
-#delete task by id button
-delTButton = Button(frame,text='Usuń zadanie po ID', command=executeDelTButon)
-delTButton.grid(row=4,column=3)
-delTButton.config(background='red', foreground='#FFFF00')
-#clear out/in area button
-clearEntryButton = Button(frame,text='Wyczyść out/in', command=executeClearOutput)
-clearEntryButton.grid(row=5,column=3)
-clearEntryButton.config(background='gray', foreground='#FFFF00')
-#change status (on/off) button
-changeStatusButton = Button(frame,text='Włącz/Wyłącz zadanie', command=executeChangeStatus)
-changeStatusButton.grid(row=4,column=1)
-changeStatusButton.config(background='blue', foreground='#FFFF00')
+    statusEntry = Entry(frame, width=40)   
+    statusEntry.grid(sticky=N,row=2,column=2,padx=5, pady=5)
+    statusLabel = Label(frame,text='Wprowadź status (1-ON | 0-OFF):') 
+    statusLabel.grid(sticky=N, row=1, column=2, padx=5, pady=5) 
 
-#change task params button
-updateTaskButton = Button(frame,text='Modyfikuj zadanie', command=executeUpdateTask)
-updateTaskButton.grid(sticky=N,row=4,column=2)
-updateTaskButton.config(background='blue', foreground='#FFFF00')
+    idTaskEntry = Entry(frame, width=40)   
+    idTaskEntry.grid(sticky=N,row=2,column=3,padx=5, pady=5)
+    idTaskLabel = Label(frame,text='Wprowadź ID zadania:') 
+    idTaskLabel.grid(sticky=N, row=1, column=3, padx=5, pady=5) 
 
-#random task button
-randomTaskButton = Button(frame,text='Losowe zadanie', command=executeRandomTask)
-randomTaskButton.grid(sticky=N,row=5,column=0)
-randomTaskButton.config(background='green', foreground='#FFFF00')
+    #BUTTONS
+    #show tasks button
+    showTButton = Button(frame,text='Pokaż pytania', command=executeShowTButon)
+    showTButton.grid(row=3,column=0)
+    showTButton.config(background='green', foreground='#FFFF00')
+    #show ON
+    showTOnButton = Button(frame,text='Pokaż włączone', command=executeShowTOnButon)
+    showTOnButton.grid(row=3,column=1)
+    showTOnButton.config(background='green', foreground='#FFFF00')
+    #show OFF
+    showTOffButton = Button(frame,text='Pokaż wyłączone', command=executeShowTOffButon)
+    showTOffButton.grid(row=3,column=2)
+    showTOffButton.config(background='green', foreground='#FFFF00')
+    #add task button
+    addTButton = Button(frame,text='Dodaj zadanie', command=executeAddTButon)
+    addTButton.grid(row=4,column=0)
+    addTButton.config(background='blue', foreground='#FFFF00')
+    #delete task by id button
+    delTButton = Button(frame,text='Usuń zadanie po ID', command=executeDelTButon)
+    delTButton.grid(row=4,column=3)
+    delTButton.config(background='red', foreground='#FFFF00')
+    #clear out/in area button
+    clearEntryButton = Button(frame,text='Wyczyść out/in', command=executeClearOutput)
+    clearEntryButton.grid(row=5,column=3)
+    clearEntryButton.config(background='gray', foreground='#FFFF00')
+    #change status (on/off) button
+    changeStatusButton = Button(frame,text='Włącz/Wyłącz zadanie', command=executeChangeStatus)
+    changeStatusButton.grid(row=4,column=1)
+    changeStatusButton.config(background='blue', foreground='#FFFF00')
 
-#sort order priority
-sortPriorityTaskButton = Button(frame,text='Od najważniejszego', command=executeSortPriority)
-sortPriorityTaskButton.grid(sticky=N,row=3,column=3)
-sortPriorityTaskButton.config(background='green', foreground='#FFFF00')
+    #change task params button
+    updateTaskButton = Button(frame,text='Modyfikuj zadanie', command=executeUpdateTask)
+    updateTaskButton.grid(sticky=N,row=4,column=2)
+    updateTaskButton.config(background='blue', foreground='#FFFF00')
 
-#output big textfield area 
-txtArea = Text(frame,width=100, height=20, font=helv36)
-txtArea.grid(sticky=W+N, row=0,column=0, padx=5, pady=5,columnspan=4) 
+    #random task button
+    randomTaskButton = Button(frame,text='Losowe zadanie', command=executeRandomTask)
+    randomTaskButton.grid(sticky=N,row=5,column=0)
+    randomTaskButton.config(background='green', foreground='#FFFF00')
+
+    #sort order priority
+    sortPriorityTaskButton = Button(frame,text='Od najważniejszego', command=executeSortPriority)
+    sortPriorityTaskButton.grid(sticky=N,row=3,column=3)
+    sortPriorityTaskButton.config(background='green', foreground='#FFFF00')
+
+    #output big textfield area 
+    txtArea = Text(frame,width=100, height=20, font=helv36)
+    txtArea.grid(sticky=W+N, row=0,column=0, padx=5, pady=5,columnspan=4)
+    
+frame1 = Frame(root,borderwidth=4)  
+frame1.grid(row=0, column=1 )
+frame1.config(background='black')
+
+te = Button(frame1,text='Foo', command=testowo)
+te.grid(row=0,column=0,padx=10,pady=5)
+te.config(background='green', foreground='#FFFF00')
+
 
 root.mainloop()
 mydb.close()
