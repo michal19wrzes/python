b18410768061daee8fdc8740be68c73f1b66a913
michal19wrzes <michal19wrzes@gmail.com>
2022-02-13 14:01:09 +0100

    minimal form

--- djangoApp/polygon/polls/urls.py
+++ djangoApp/polygon/polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('',views.index,name='index'),
     path('<int:question_id>/results/',views.results,name='results'),
