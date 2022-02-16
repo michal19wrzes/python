c15ffa9ed1dbff01558abd7c3ff0f21b1a7ad171
michal19wrzes <michal19wrzes@gmail.com>
2022-02-16 20:09:16 +0100

    tests

--- djangoApp/polygon/polls/models.py
+++ djangoApp/polygon/polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 # Create your models here.
 
 class Question(models.Model):
@@ -6,7 +8,12 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
         
     def __str__(self):
         return self.question_text
