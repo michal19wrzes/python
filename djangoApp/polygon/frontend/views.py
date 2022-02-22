6280a935d56c9c293cde899d6ba34cfd214152e7
michal19wrzes <michal19wrzes@gmail.com>
2022-02-22 19:19:11 +0100

    connected react

--- djangoApp/polygon/frontend/views.py
+++ djangoApp/polygon/frontend/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request, *args, **kwargs):
+    return render(request,'C:\\Users\\48791\\Desktop\\IT\\pythonowe\\djangoApp\\polygon\\frontend\\templates\\frontend\\index.html')
