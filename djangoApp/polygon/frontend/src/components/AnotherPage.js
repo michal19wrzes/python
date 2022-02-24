8d5bfbd7e29af1de62196d2278d69346dacb53b8
michal19wrzes <michal19wrzes@gmail.com>
2022-02-24 16:12:02 +0100

    mui integration

--- djangoApp/polygon/frontend/src/components/AnotherPage.js
+++ djangoApp/polygon/frontend/src/components/AnotherPage.js
@@ -1,4 +1,14 @@
 import React, { Component } from 'react';
+import Button from '@mui/material/Button';
+import { link } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/Button';
 
 export default class AnotherPage extends Component {
 	constructor(props) {
@@ -6,6 +16,20 @@ export default class AnotherPage extends Component {
 	}
 	
 	render(){
-		return <h1>ELO</h1>;
+		return (
+			<Grid container spacing={1}>
+				<Grid item xs={12} align="center">
+					<Typography component="h4" variant="h4">
+						Test typography
+					</Typography>
+					<Typography component="h4" variant="h4">
+						Test typography
+					</Typography>
+					<Typography component="h4" variant="h4">
+						Test typography
+					</Typography>
+				</Grid>
+			</Grid>
+		);
 	}
 }
