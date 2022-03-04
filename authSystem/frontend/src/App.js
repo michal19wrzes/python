80b09251e7b8c85a5b20d47ac89097191d230d8b
michal19wrzes <michal19wrzes@gmail.com>
2022-03-05 00:44:31 +0100

    react mui integration

--- authSystem/frontend/src/App.js
+++ authSystem/frontend/src/App.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import Home from './containers/Home';
 import Signup from './containers/Signup';
 import ResetPassword from './containers/ResetPassword';
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
 import Login from './containers/Login';
 import Activate from './containers/Activate';
 import Navbar from './containers/Navbar';
@@ -22,8 +23,8 @@ export default class App extends Component {
 					<Route exact path='/signup' element={<Signup />} />
 					<Route exact path='/reset-password' element={<ResetPassword />} />
 					<Route exact path='/login' element={<Login />} />
-					<Route exact path='/activate' element={<Activate />} />
-					
+					<Route exact path='/activate/:uid/:token' element={<Activate />} />
+					<Route exact path='password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm />} />
 					<Route path='/another' element={<h1>This is another</h1>} />
 				</Routes>
 				
--- authSystem/frontend/src/containers/Home.js
+++ authSystem/frontend/src/containers/Home.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
 
 const Home = (props) => (
 		<div>
-			<Navbar />
-			<h1>home</h1> 
+			<Stack spacing ={2} align = 'center'>
+				<Navbar />
+				<h1>home</h1>
+				<Typography component="h4" variant="h4">
+					essa
+					
+				</Typography>
+				<Button color="secondary" variant="contained" to="/" component={Link}>
+					Back
+				  </Button>
+			</Stack>
 		</div>
 );
 
