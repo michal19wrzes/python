8e7a8e2e34ee2016a116d740af022e4600565f06
michal19wrzes <michal19wrzes@gmail.com>
2022-03-04 23:54:49 +0100

    update

---
+++ authSystem/frontend/src/containers/ResetPasswordConfirm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const ResetPasswordConfirm = (props) => (
+		<div>
+			<Navbar />
+			<h1>ResetPasswordConfirm</h1> 
+		</div>
+);
+
+export default ResetPasswordConfirm;
