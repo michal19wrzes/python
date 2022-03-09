8a4bed120c2717603c6c967ae69d12b5e245ccc1
michal19wrzes <michal19wrzes@gmail.com>
2022-03-09 19:32:59 +0100

    problem with JSON parsing auth credentials

--- authSystem/backend/auth_system/settings.py
+++ authSystem/backend/auth_system/settings.py
@@ -93,7 +93,7 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "michal19wrz@gmail.com"
-EMAIL_HOST_PASSWORD = "testowehaslo"
+EMAIL_HOST_PASSWORD = "testowehaslo1"
 EMAIL_USE_TLS = True
 
 # Password validation
