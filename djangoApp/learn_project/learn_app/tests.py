1b2425089e11f43035cdcd27bb8a63925acf8f08
michal19wrzes <michal19wrzes@gmail.com>
2022-05-02 19:11:05 +0200

    problem with factory_boy

--- djangoApp/learn_project/learn_app/tests.py
+++ djangoApp/learn_project/learn_app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from .models import Kucharz,Potrawa
+from .factories import KucharzFactory
 # Create your tests here.
+class KucharzTestCase(TestCase):
+    def setUp(self):
+        self.kucharze = KucharzFactory.build_batch(3)
+        
+
+    def test_czy_michal_programista(self):
+        self.assertTrue(False,'Michal jest programista')
