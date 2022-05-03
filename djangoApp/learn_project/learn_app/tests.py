9c81e628625eea333e4909df21824b9c4e4dab2b
michal19wrzes <michal19wrzes@gmail.com>
2022-05-03 20:19:18 +0200

    'tests'

--- djangoApp/learn_project/learn_app/tests.py
+++ djangoApp/learn_project/learn_app/tests.py
@@ -1,11 +1,19 @@
 from django.test import TestCase
 from .models import Kucharz,Potrawa
-from .factories import KucharzFactory
+from .factories import KucharzFactory, PotrawaFactory
 # Create your tests here.
 class KucharzTestCase(TestCase):
     def setUp(self):
         self.kucharze = KucharzFactory.build_batch(3)
-        
+        self.potrawy = PotrawaFactory.build_batch(3)
 
-    def test_czy_michal_programista(self):
-        self.assertTrue(True,'Michal jest programista')
+    def test_kucharz_creation(self):
+        for kucharz in self.kucharze:
+            kucharz.save()
+        self.assertIsNotNone(kucharz.id)
+
+    def test_potrawa_creation(self):
+        for potrawa in self.potrawy:
+            potrawa.powi.save()
+            potrawa.save()
+            self.assertIsNotNone(potrawa.id)
