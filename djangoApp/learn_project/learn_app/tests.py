c2fad9caebc366b6dd01cb917f88b9ce01a2c2a3
michal19wrzes <michal19wrzes@gmail.com>
2022-05-03 21:16:15 +0200

    fix PotrawaTestCase

--- djangoApp/learn_project/learn_app/tests.py
+++ djangoApp/learn_project/learn_app/tests.py
@@ -3,14 +3,20 @@ from .models import Kucharz,Potrawa
 from .factories import KucharzFactory, PotrawaFactory
 # Create your tests here.
 class KucharzTestCase(TestCase):
+    
     def setUp(self):
         self.kucharze = KucharzFactory.build_batch(3)
-        self.potrawy = PotrawaFactory.build_batch(3)
 
     def test_kucharz_creation(self):
         for kucharz in self.kucharze:
             kucharz.save()
         self.assertIsNotNone(kucharz.id)
+
+
+class PotrawaTestCase(TestCase):
+
+    def setUp(self):
+        self.potrawy = PotrawaFactory.build_batch(3)
 
     def test_potrawa_creation(self):
         for potrawa in self.potrawy:
