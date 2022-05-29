0b0418f330d4efd3ba1d5d85cc9cf750502ce440
michal19wrzes <michal19wrzes@gmail.com>
2022-05-29 12:23:39 +0200

    fix

--- learn_py/spoj_python/medium/trnglfld.py
+++ learn_py/spoj_python/medium/trnglfld.py
@@ -1,27 +1,37 @@
 import math
 
-t = int(input("T:"))
+#print 3 sorted elements for divided len via 3 list
+def splitList(l):
+  x = len(l)//3
+  for i in range(x):
+    temp=[]
+    temp.append(l[i*3])
+    temp.append(l[i*3+1])
+    temp.append(l[i*3+2])
+    temp.sort()
+    print(f"{temp[0]} {temp[1]} {temp[2]}")
+
+t = int(input())
 for test in range(t):
-  cov = int(input("cov:"))
+  cov = int(input())
   inData=[]
-  odl=[]
+  dist=[]
+  outData={}
   
   for v in range(cov):
-    inData.append(input(f"v{v}:").split())
-    print(inData[v])
-
+    inData.append(input().split())
   for data in inData:
-    staraOdl=0
-    print(data)
-    for lata in inData:
-      
+    maxDist=0
+    for lata in inData: #
       r1 = int(lata[1])-int(data[1])
       r2 = int(lata[0])-int(data[0])
-      
-      d = math.sqrt(r1*r1+r2*r2)
-      print(f"odleglosc: {data} od: {lata} to: {d}")
-      if d>staraOdl:
-        staraOdl=d
-        
-      odl.append(d)
-    print(f"maxOdl:{staraOdl}")
+      d = math.sqrt(r1*r1+r2*r2) #distance from p to p
+
+      if d>maxDist:
+        maxDist=d
+    dist.append(maxDist)    
+
+  for i in range(len(inData)):
+    outData[str(i+1)]=dist[i]
+  outData = {k:v for k,v in sorted(outData.items(),key=lambda x: x[1], reverse=True)}
+  splitList(list(outData.keys()))
