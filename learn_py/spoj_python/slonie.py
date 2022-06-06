b930fd7b79e7f1c3c5f63310b8abc9525c2349d8
michal19wrzes <michal19wrzes@gmail.com>
2022-06-06 16:22:04 +0200

    perfect

--- learn_py/spoj_python/slonie.py
+++ learn_py/spoj_python/slonie.py
@@ -1,49 +1,32 @@
 import numpy as np
+import fileinput
 from numpy import int64
 
-path = input() #input filename
-with open(path,'r') as file:
-    count_of_elephants = int(file.readline())
-    masses_of_elephants = np.array(file.readline().split(), dtype=int64)
-    input_permutation = np.array(file.readline().split(), dtype=int64)
-    director_permutation = np.empty(count_of_elephants, dtype=int64)
+ins = list(fileinput.input())
+count_of_elephants = int(ins[0])
+masses_of_elephants = np.array(ins[1].split(), dtype=int64)
+min_weight = min(masses_of_elephants)
+input_permutation = np.array(ins[2].split(), dtype=int64)
+director_permutation = np.empty(count_of_elephants, dtype=int64)
 
-    i = 0
-    for number in file.readline().split():
-        director_permutation[ int(number) - 1 ] = input_permutation[i]
-        i += 1
+i = 0
+for number in ins[3].split():
+    director_permutation[ int(number) - 1 ] = input_permutation[i]
+    i += 1
 
 odw = [False for x in range(count_of_elephants+1)]
-count_of_cycles = 0
-sum_of_elephant_in_cycle =0
-sums_of_cycles =[]
-indices_in_cycles = []
-min_vals_of_cycles=[]
-
-for i in range(1,count_of_elephants+1):
+score = 0
+for i in range(1,count_of_elephants):
     if not odw[i]:
-        indices_in_cycle = []
-        count_of_cycles +=1
+        weights_of_elephtants = 0
+        length_of_cycle = 0
+        min_of_cycle = float('inf')
         x=i
         while not odw[x]:
             odw[x]=True
-            sum_of_elephant_in_cycle += masses_of_elephants[x-1]
+            weights_of_elephtants += masses_of_elephants[x-1]
             x = director_permutation[x-1]
-            indices_in_cycle.append(x)
-
-        sums_of_cycles.append(sum_of_elephant_in_cycle)
-        indices_in_cycles.append(indices_in_cycle)
-        sum_of_elephant_in_cycle =0
-
-for i in range(count_of_cycles):
-    minimum = float('inf')
-    for e in indices_in_cycles[i]:
-        minimum = min(minimum,masses_of_elephants[e-1])
-    min_vals_of_cycles.append(minimum)
-
-w = 0
-for i in range(count_of_cycles):
-    method1=sums_of_cycles[i] + (len(indices_in_cycles[i])-2) * min_vals_of_cycles[i]
-    method2=sums_of_cycles[i] + min_vals_of_cycles[i] + (len(indices_in_cycles[i])+1) * min(min_vals_of_cycles)
-    w += min(method1,method2)
-print(w)
+            min_of_cycle = min(min_of_cycle,masses_of_elephants[x-1])
+            length_of_cycle += 1
+        score += min(weights_of_elephtants + (length_of_cycle - 2) * min_of_cycle, weights_of_elephtants + min_of_cycle + (length_of_cycle + 1) * min_weight)
+print(score, end='')
